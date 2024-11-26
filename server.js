243d50b6f1b86be52e824baff0a9f755058ea7ed
Michael Brookins <michael.brookins@rfsmart.com>
2024-11-26 15:06:17 -0500

    CRUD ops

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');.
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+// const posts = require('./routes/posts');
 const port = process.env.PORT || 8000; 
 
-//setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-// app.get('/', (req, res)=> {
-//     // res.send('<h1>Hello World!<h1>'); 
-//     // res.send({message: 'Hello World!'}); 
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+//Routes
+app.use('/api/posts', posts);
 
-// app.get('/about', (req, res)=> {
-//     // res.send('About'); 
-//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
-// });
-
-let posts = [
-    {id:1, title: 'post1'},
-    {id:2, title: 'post2'},
-    {id:3, title: 'post3'}
-];
-
-app.get('/api/posts', (req, res) => {
-    res.json(posts);
-});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+//56:13 - https://www.youtube.com/watch?v=CnH3kAXSrmU
+
+
 
