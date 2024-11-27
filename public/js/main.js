9a4e7b3bb5052aacdf5e23674ab8ac75b4dffc3a
Michael Brookins <michael.brookins@rfsmart.com>
2024-11-27 11:56:11 -0500

    __dir workaround

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+const output = document.querySelector('#output');
+const button = document.querySelector('#get-posts-btn');
+
+//1:26 https://www.youtube.com/watch?v=CnH3kAXSrmU
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import path from 'path';
+import {fileURLToPath} from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js'
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
 
-
 const port = process.env.PORT || 8000; 
+
+//Get directory name
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__fileName);
+
 const app = express();
 
 //Body parser middleware
@@ -15,6 +20,10 @@ app.use(express.urlencoded({extended: false}));
 
 //logger middleware
 app.use(logger);
+
+//Setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //Routes
 app.use('/api/posts', posts);
@@ -34,6 +43,5 @@ app.use(notFound);
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
-//1:20 https://www.youtube.com/watch?v=CnH3kAXSrmU
 
 
